f3188c78909db8ca627932bb5da9384cfe34f236
Quinnss <2183907473@qq.com>
2026-02-12 12:12:23 +0800

    后台登录页面美化

--- src/pages/account/login/index.vue
+++ src/pages/account/login/index.vue
@@ -1,30 +1,32 @@
 <template>
-    <div class="page-account">
+    <div class="page-account page-account--cute">
         <div v-if="showI18n" class="page-account-header">
             <i-header-i18n />
         </div>
-        <div class="page-account-container">
-            <div class="page-account-top">
-                <div class="page-account-top-logo">
-                    <img src="@/assets/images/logo.png" alt="logo">
+        <div class="page-account-card">
+            <div class="page-account-hero">
+                <div class="hero-mascot">
+                    <img src="@/assets/images/login-mascot.png" alt="宠物之家" />
                 </div>
-                <div class="page-account-top-desc">iView Admin Pro 企业级中台前端/设计解决方案</div>
+                <div class="hero-title">欢迎来到宠物之家</div>
+                <div class="hero-subtitle">您的关爱，我们的使命</div>
             </div>
-            <Login @on-submit="handleSubmit">
-                <UserName name="username" value="admin" />
-                <Password name="password" value="admin" enter-to-submit />
-                <div class="page-account-auto-login">
-                    <Checkbox v-model="autoLogin" size="large">{{ $t('page.login.remember') }}</Checkbox>
-                    <a href="">{{ $t('page.login.forgot') }}</a>
+            <div class="page-account-form">
+                <div class="form-title">宠物之家</div>
+                <div class="form-subtitle">PET HOME</div>
+                <div class="form-welcome">欢迎回来！</div>
+                <Login @on-submit="handleSubmit">
+                    <UserName name="username" value="admin" />
+                    <Password name="password" value="admin" enter-to-submit />
+                    <div class="page-account-auto-login">
+                        <Checkbox v-model="autoLogin" size="large">{{ $t('page.login.remember') }}</Checkbox>
+                        <a href="">{{ $t('page.login.forgot') }}</a>
+                    </div>
+                    <Submit>{{ $t('page.login.submit') }}</Submit>
+                </Login>
+                <div class="page-account-other">
+                    <router-link class="page-account-register" :to="{ name: 'register' }">立即注册</router-link>
                 </div>
-                <Submit>{{ $t('page.login.submit') }}</Submit>
-            </Login>
-            <div class="page-account-other">
-                <span>{{ $t('page.login.other') }}</span>
-                <img src="@/assets/svg/icon-social-wechat.svg" alt="wechat">
-                <img src="@/assets/svg/icon-social-qq.svg" alt="qq">
-                <img src="@/assets/svg/icon-social-weibo.svg" alt="weibo">
-                <router-link class="page-account-register" :to="{ name: 'register' }">{{ $t('page.login.signup') }}</router-link>
             </div>
         </div>
         <i-copyright />
